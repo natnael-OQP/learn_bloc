27a2937adf11cca2690c1a1d1e1774fc3b02a25d
natnael-OQP <makiya112545@gmail.com>
2022-08-16 18:21:41 +0300

    style codes

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -5,16 +5,8 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(value: 0, isIncremented: false));
 
-  void increment() => emit(
-        CounterState(
-          value: state.value + 1,
-          isIncremented: true,
-        ),
-      );
-  void decrement() => emit(
-        CounterState(
-          value: state.value - 1,
-          isIncremented: false,
-        ),
-      );
+  void increment() =>
+      emit(CounterState(value: state.value + 1, isIncremented: true));
+  void decrement() =>
+      emit(CounterState(value: state.value - 1, isIncremented: false));
 }
